f2aeaf677fe90bdb02e9cf5103e6441abd147ad9
Joonas Arola <joonas.arola@tuni.fi>
2020-04-26 09:23:08 +0300

    Aloiettu käyttöliittymä.

---
+++ harjoitustyo/kayttoliittyma/Käyttöliittymä.java
@@ -0,0 +1,12 @@
+package harjoitustyo.kayttoliittyma;
+/**
+ * Konkreettinen luokka käyttöliittymälle.
+ * <p>
+ * Olio-ohjelmoinnin perusteet II, kevät 2020.
+ * 
+ * @version 0.1
+ * @author Joonas Arola, joonas.arola@tuni.fi.
+ */
+public class Käyttöliittymä {
+    
+}
